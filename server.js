a3cc6cec7a1dd5138c27c30736cf64cf18850657
Ulysse Mensa <ulysse.mensa@si-s.fr>
2016-09-27 16:08:54 +0200

    wrong librairy

--- server.js
+++ server.js
@@ -2,25 +2,19 @@
 //app.use(logger('dev'));
 //var debug = require('debug')('monit-O-ring_back-end:server');
 var os     = require('os-utils');
-var usage  = require('os-usage');  
 var cpu    = require('./cpu');
 var conf   = require('./config/conf');
 var socket = require('socket.io-client')(conf.url_front_end);
 
 socket.on('connect', function(){
 
-	var cpusData = null;
-	var cpuMonitor = new usage.CpuMonitor();
-
     socket.emit('identifier', { serverName: conf.server_url });
         
     var usedMemory = os.totalmem() - os.freemem();
 
 	// watch cpu usage overview
-	cpuMonitor.on('cpuUsage', function(data) {
-	    console.log(data);
-
-	    // { user: '9.33', sys: '56.0', idle: '34.66' }
+	os.cpuUsage(function(v){
+	    console.log( 'CPU Usage (%): ' + v );
 	});
 
 	socket.emit('ehlo', { 
