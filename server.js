ab93c2e802192e096d249237366607d6a2f053e4
Ulysse Mensa <ulysse.mensa@si-s.fr>
2016-09-27 15:40:31 +0200

    test new os utils and cpu usage tools

--- server.js
+++ server.js
@@ -1,46 +1,53 @@
 //var logger = require('morgan');
 //app.use(logger('dev'));
 //var debug = require('debug')('monit-O-ring_back-end:server');
-var os     = require('os');  
+var os     = require('os-utils');
+var usage  = require('os-usage');  
 var cpu    = require('./cpu');
 var conf   = require('./config/conf');
 var socket = require('socket.io-client')(conf.url_front_end);
-// const mongoose = require('mongoose');
 
-// var db = require('./config/db');
-// mongoose.connect(db.url, function(err) {
-//   	if (err) { throw err; }
-// });
+var cpuMonitor = new usage.CpuMonitor();
+
 socket.on('connect', function(){
 
     socket.emit('identifier', { serverName: conf.server_url });
         
-    var totalMemory = Number(os.totalmem() / 1073741824).toFixed(4);
-    var freeMemory = Number(os.freemem() / 1073741824).toFixed(4);
-    var usedMemory = Number((os.totalmem() - os.freemem()) / 1073741824).toFixed(4);
+    var usedMemory = os.totalmem() - os.freemem();
 
-    cpu().then(function(cpuPercentage) {
-        socket.emit('ehlo', { 
-            cpu: cpuPercentage,
-            totalMemory: totalMemory,
-            freeMemory: freeMemory,
+    cpuMonitor.on('cpuUsage', function(data) {
+	    socket.emit('ehlo', { 
+            cpu: data,
+            totalMemory: os.totalmem(),
+            freeMemory: os.freemem(),
             usedMemory: usedMemory,
-            uptime: os.uptime(),
+            uptime: os.sysUptime();,
             hostname: conf.server_url
         });
-    });
+
+	    // { user: '9.33', sys: '56.0', idle: '34.66' }
+	});
+
+    // cpu().then(function(cpuPercentage) {
+    //     socket.emit('ehlo', { 
+    //         cpu: cpuPercentage,
+    //         totalMemory: os.totalmem(),
+    //         freeMemory: os.freemem(),
+    //         usedMemory: usedMemory,
+    //         uptime: os.sysUptime();,
+    //         hostname: conf.server_url
+    //     });
+    // });
 
     setInterval(() => {
         cpu().then(function(cpuPercentage) {
-            totalMemory = Number(os.totalmem() / 1073741824).toFixed(4);
-            freeMemory = Number(os.freemem() / 1073741824).toFixed(4);
-            usedMemory = Number((os.totalmem() - os.freemem()) / 1073741824).toFixed(4);
+    		usedMemory = os.totalmem() - os.freemem();
             socket.emit('ressources', { 
                 cpu: cpuPercentage,
-                totalMemory: totalMemory,
-                freeMemory: freeMemory,
+                totalMemory: os.totalmem(),
+                freeMemory: os.freemem(),
                 usedMemory: usedMemory,
-                uptime: os.uptime(),
+                uptime: os.sysUptime();,
                 hostname: conf.server_url
             });
         });
